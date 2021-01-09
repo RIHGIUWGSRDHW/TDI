ef0bfd8ddd1df711d86dc40e3f7c4aa042aa62ab
RIHGIUWGSRDHW <76130949+RIHGIUWGSRDHW@users.noreply.github.com>
2021-01-09 16:40:38 +0100

    Update La_Cucaracha_Ejemplo.ino

--- La Cucaracha/La_Cucaracha_Ejemplo/La_Cucaracha_Ejemplo.ino
+++ La Cucaracha/La_Cucaracha_Ejemplo/La_Cucaracha_Ejemplo.ino
@@ -1,8 +1,8 @@
-int piezoPin = 8;
+int piezoPin = 16;
 int tones[] = {196, 196, 196, 261.63, 329.63, 196, 196, 196, 261.63, 329.63, 261.63, 261.63, 246.94, 246.94, 220, 220, 196, 196, 196, 196, 246.94, 293.66, 196, 196, 196, 246.94, 293.66, 392, 440, 392, 349.23, 329.63, 293.66, 261.63};
 int beats[] = {1, 1, 1, 2, 1, 1, 1, 1, 2, 4, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 2, 1, 1, 1, 1, 2, 4, 1, 1, 1, 1, 1, 1, 3};
 int Length = 34;
-int tempo = 300;
+int tempo = 250;
 
 void setup() {
   pinMode(piezoPin, OUTPUT);
@@ -11,9 +11,10 @@ void setup() {
 void loop() {
   for(int i=0; i<Length; i++){
     noTone(piezoPin);
+    delay(35);
     tone(piezoPin, tones[i]);
     delay(tempo*beats[i]);
   }
   noTone(piezoPin);
-  delay(2000000);
+  delay(100000000);
 }
